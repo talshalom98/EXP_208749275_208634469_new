e436351394c9eb01fd81582fa30a4559c81ca50c
mshiloni22 <mshiloni22@gmail.com>
2023-07-07 18:21:29 +0300

    Initial commit

---
+++ src/Manager.java
@@ -0,0 +1,22 @@
+public class Manager {
+    // your code here
+
+    public Manager() {
+        //your code here
+    }
+
+    public void readInput(String path) {
+        // your code here
+    }
+
+    public String start() {
+        // your code here
+        return "coloring massage in output format";
+
+    }
+
+    public String terminate() {
+        // your code here
+        return "coloring massage in output format";
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    private int id;
+    private int color;
+    private int numNodes;
+    private int maxDeg;
+    //your code here
+
+    public Node(int id, int numNodes,int maxDeg,int[][] neighbors){
+        this.id = id;
+        this.color = id;
+        this.numNodes = numNodes;
+        this.maxDeg = maxDeg;
+        // your code here
+    }
+
+    public void run(){
+        // your code here
+    }
+}
