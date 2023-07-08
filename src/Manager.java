3f00308cedec410392664447605e72940606e9e3
mshiloni22 <mshiloni22@gmail.com>
2023-07-08 17:39:58 +0300

    Write structure of Manager and Node classes. Code currently fail on reading input and creating threads.

--- src/Manager.java
+++ src/Manager.java
@@ -1,22 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Manager {
-    // your code here
+    private int numNodes;
+    private int maxDeg;
+    private List<Node> nodes;
 
     public Manager() {
-        //your code here
+        nodes = new ArrayList<>();
     }
 
     public void readInput(String path) {
-        // your code here
+        try {
+            Scanner scanner = new Scanner(new File(path));
+            numNodes = scanner.nextInt();
+            maxDeg = scanner.nextInt();
+
+            for (int i = 0; i < numNodes; i++) {
+                int nodeId = i;
+                int[][] neighborsInfo = parseNeighborsInfo(scanner);
+                Node node = new Node(nodeId, numNodes, maxDeg, neighborsInfo);
+                nodes.add(node);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: Input file not found.");
+            e.printStackTrace();
+        }
+    }
+
+    private int[][] parseNeighborsInfo(Scanner scanner) {
+        String neighborsString = scanner.nextLine();
+        neighborsString = neighborsString.substring(2);
+        String[] neighborsArray = neighborsString.split("\\], \\[");
+
+        int numNeighbors = neighborsArray.length;
+        int[][] neighbors = new int[numNeighbors][3];
+
+        for (int i = 0; i < numNeighbors; i++) {
+            String neighborInfo = neighborsArray[i];
+            String[] neighborParts = neighborInfo.split(", ");
+
+            int neighborId = Integer.parseInt(neighborParts[0]);
+            int writingPort = Integer.parseInt(neighborParts[1]);
+            int readingPort = Integer.parseInt(neighborParts[2]);
+
+            neighbors[i][0] = neighborId;
+            neighbors[i][1] = writingPort;
+            neighbors[i][2] = readingPort;
+        }
+
+        return neighbors;
     }
 
     public String start() {
-        // your code here
-        return "coloring massage in output format";
+        Thread[] threads = new Thread[numNodes];
+        for (int i = 0; i < numNodes; i++) {
+            final int nodeId = i;
+            Node node = nodes.get(nodeId);
+            threads[nodeId] = new Thread(() -> node.run());
+            threads[nodeId].start();
+        }
 
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Error: Thread interrupted.");
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Node node : nodes) {
+            sb.append(node.toString()).append("\n");
+        }
+        return sb.toString().trim();
     }
 
     public String terminate() {
-        // your code here
-        return "coloring massage in output format";
+        StringBuilder sb = new StringBuilder();
+        for (Node node : nodes) {
+            node.terminate();
+            sb.append(node.toString()).append("\n");
+        }
+        return sb.toString().trim();
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,19 +1,103 @@
-public class Node {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
+public class Node implements Runnable {
     private int id;
     private int color;
     private int numNodes;
     private int maxDeg;
-    //your code here
+    private List<int[]> neighbors;
+    private volatile boolean isTerminated;
 
-    public Node(int id, int numNodes,int maxDeg,int[][] neighbors){
+    public Node(int id, int numNodes, int maxDeg, int[][] neighbors) {
         this.id = id;
         this.color = id;
         this.numNodes = numNodes;
         this.maxDeg = maxDeg;
-        // your code here
+        this.neighbors = List.of(neighbors);
+        this.isTerminated = false;
     }
 
-    public void run(){
-        // your code here
+    @Override
+    public void run() {
+        boolean isColoringValid = false;
+        int iterations = 0;
+
+        while (!isColoringValid && !isTerminated) {
+            iterations++;
+
+            for (int[] neighbor : neighbors) {
+                int neighborId = neighbor[0];
+                int writingPort = neighbor[1];
+                int readingPort = neighbor[2];
+
+                sendMessage(neighborId, writingPort, id, color);
+
+                int receivedId = receiveMessage(readingPort);
+                int receivedColor = receiveMessage(readingPort);
+
+                if (receivedId == neighborId && receivedColor == color) {
+                    color = (color + 1) % (maxDeg + 1);
+                }
+            }
+
+            isColoringValid = checkColoringValidity();
+
+            if (iterations >= numNodes) {
+                break;
+            }
+        }
+    }
+
+    private void sendMessage(int neighborId, int writingPort, int id, int color) {
+        try (Socket socket = new Socket("localhost", writingPort);
+             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+
+            out.writeInt(id);
+            out.writeInt(color);
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int receiveMessage(int readingPort) {
+        try (Socket socket = new Socket("localhost", readingPort);
+             ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+            return in.readInt();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    private boolean checkColoringValidity() {
+        for (int[] neighbor : neighbors) {
+            int neighborId = neighbor[0];
+            int readingPort = neighbor[2];
+            int neighborColor = receiveMessage(readingPort);
+
+            if (neighborColor == color) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void terminate() {
+        isTerminated = true;
+    }
+
+    @Override
+    public String toString() {
+        return id + "," + color;
     }
 }
